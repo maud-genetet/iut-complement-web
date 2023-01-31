b78a1aaccd99e00e7663dd360e335a4df70c861f
CaroleLf <carole.56leflohic@gmail.com>
2023-01-31 10:38:20 +0100

    ex5

--- TD1/ex2.js
+++ TD1/ex2.js
@@ -14,6 +14,5 @@ function changeBackground() {
 function changeText(){
     document.getElementById("secondLink").innerHTML = "coucou";
     document.getElementById("secondLink").style.backgroundColor = "orange"
-    
 }
    
